db01a7b29845f0329f2a33a20e7b9113455eb995
honzanugeta <100222360+honzanugeta@users.noreply.github.com>
2024-11-26 21:57:10 +0100

    potrebuji to cely predelat (github)

--- VietnamecSimulator/Assets/Scripts/GameManager.cs
+++ VietnamecSimulator/Assets/Scripts/GameManager.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using Unity.VisualScripting;
 using UnityEngine;
 using MouseButton = UnityEngine.UIElements.MouseButton;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] public GameObject PauseMenu;
+    [SerializeField] public GameObject PlayerCamera;
+    private bool PauseActive = false;
     
     // Start is called before the first frame update
     void Start()
-    {
+    {      
         Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        PauseActive = !PauseActive;
+        PauseMenu.SetActive(PauseActive);
+        Time.timeScale = PauseActive ? 0 : 1;
     }
 }

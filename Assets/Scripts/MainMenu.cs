b10593f346584bdbfe6cc61b054f69d3c299854e
honzanugeta <100222360+honzanugeta@users.noreply.github.com>
2024-11-27 00:02:23 +0100

    neco jsem posral

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]  RectTransform Logo;
+    [SerializeField]  float topposy, middleposy;
+    [SerializeField]  float tweenDuration;
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        LogoLoad();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LogoLoad()
+    {
+        
+    }
+
+   
+}

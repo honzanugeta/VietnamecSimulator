bb754ee5593690f15e021b584d60286686f2e6c0
honzanugeta <100222360+honzanugeta@users.noreply.github.com>
2025-03-04 22:55:44 +0100

    some minor updates

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,7 +15,6 @@ public class GameManager : MonoBehaviour
     [SerializeField] public GameObject OrderingTablet;
     [SerializeField] private GameObject inventoryUI;
     private bool PauseActive = false;
-
     
     // Start is called before the first frame update
     void Start()
